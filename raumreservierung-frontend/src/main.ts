5e1dbd874493b8e44d5c4c8232a04b4b9cfd5edf
langehm <38280183+langehm@users.noreply.github.com>
2026-02-09 14:49:51 +0100

    :truck: renamend privilege action self:read to self, as the user can always edit his entities

--- raumreservierung-frontend/src/main.ts
+++ raumreservierung-frontend/src/main.ts
@@ -5,11 +5,8 @@ import { registerPlugins } from "@/plugins";
 
 import "unfonts.css";
 
-import { registerDirectives } from "@/directives";
-
 const app = createApp(App);
 
 registerPlugins(app);
-registerDirectives(app);
 
 app.mount("#app");
