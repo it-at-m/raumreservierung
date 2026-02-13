a6f73f4254c80e4e4c02a6f4a8b57a52e230ec78
langehm <38280183+langehm@users.noreply.github.com>
2026-02-13 09:53:20 +0100

    moved navList and outsourced individual items

---
+++ raumreservierung-frontend/src/components/common/NavigationDrawerItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-list-item
+    v-if="navEntry.kind === 'item'"
+    color="tertiary"
+    :value="navEntry.id"
+    :to="navEntry.to"
+  >
+    <v-list-item-title>
+      {{ navEntry.textKey }}
+    </v-list-item-title>
+  </v-list-item>
+  <v-list-group v-else>
+    <template #activator="{ props }">
+      <v-list-item
+        v-bind="props"
+        :title="navEntry.textKey"
+      />
+    </template>
+    <navigation-drawer-item
+      v-for="subEntry in navEntry.children"
+      :key="subEntry.id"
+      :nav-entry="subEntry"
+    />
+  </v-list-group>
+</template>
+
+<script setup lang="ts">
+import type { NavEntry } from "@/types/NavigationEntries.ts";
+
+defineProps<{ navEntry: NavEntry }>();
+</script>
+
+<style scoped></style>
---
+++ raumreservierung-frontend/src/components/common/NavigationDrawerList.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-list>
+    <navigation-drawer-item
+      v-for="navEntry in navEntries"
+      :key="navEntry.id"
+      :nav-entry="navEntry"
+    />
+  </v-list>
+</template>
+
+<script setup lang="ts">
+import NavigationDrawerItem from "@/components/common/NavigationDrawerItem.vue";
+import { useNavEntries } from "@/composables/useNavEntries.ts";
+
+const navEntries = useNavEntries();
+</script>
+
+<style scoped></style>
