dcd6419b82af9a0e096b165eac1b89bfe1503b01
langehm <38280183+langehm@users.noreply.github.com>
2026-02-13 09:54:34 +0100

    introduced type for navItems

---
+++ raumreservierung-frontend/src/types/NavigationEntries.ts
@@ -0,0 +1,24 @@
+import type { Privilege } from "@/types/Privilege.ts";
+import type { RouteLocationRaw } from "vue-router";
+
+type NavKind = "item" | "group";
+
+interface NavBase {
+  id: string;
+  textKey: string;
+  icon?: string;
+  requiredPrivilege?: Privilege;
+  kind: NavKind;
+}
+
+export interface NavItem extends NavBase {
+  kind: "item";
+  to: RouteLocationRaw;
+}
+
+export interface NavGroup extends NavBase {
+  kind: "group";
+  children: readonly NavItem[];
+}
+
+export type NavEntry = NavItem | NavGroup;
