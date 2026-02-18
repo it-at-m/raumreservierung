74991f6760109e0d963c4e4fc2d334a3ece16876
flahamue <232542105+flahamue@users.noreply.github.com>
2026-02-18 14:06:15 +0100

    deleted unused methods and import statementsf

--- raumreservierung-backend/src/main/java/de/muenchen/raumreservierung/holidays/HolidayService.java
+++ raumreservierung-backend/src/main/java/de/muenchen/raumreservierung/holidays/HolidayService.java
@@ -1,12 +1,8 @@
 package de.muenchen.raumreservierung.holidays;
 
-import static de.muenchen.raumreservierung.common.ExceptionMessageConstants.MSG_NOT_FOUND;
-
-import de.muenchen.raumreservierung.common.NotFoundException;
 import java.util.List;
 import java.util.UUID;
 
-import de.muenchen.raumreservierung.holidays.dto.HolidayRequestDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -16,17 +12,6 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class HolidayService {
     private final HolidayRepository holidayRepository;
-
-    public Holiday getHoliday(final UUID holidayId) {
-        log.info("Get TheEntity with ID {}", holidayId);
-        return getHolidayOrThrowException(holidayId);
-    }
-
-    private Holiday getHolidayOrThrowException(final UUID holidayId) {
-        return holidayRepository
-                .findById(holidayId)
-                .orElseThrow(() -> new NotFoundException(String.format(MSG_NOT_FOUND, holidayId)));
-    }
 
     public List<Holiday> getPublicHolidays() {
         return holidayRepository.findPublicHolidays();
