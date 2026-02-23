18db4225e242a5949edeb60a398ce3e30254fd7d
langehm <38280183+langehm@users.noreply.github.com>
2026-02-23 15:29:53 +0100

    added api composables

---
+++ raumreservierung-frontend/src/composables/api/useEquipmentApi.ts
@@ -0,0 +1,42 @@
+import type {
+  DeleteEquipmentRequest,
+  EquipmentResponseDto,
+  SaveEquipmentRequest,
+  UpdateEquipmentRequest,
+} from "@/api/raumreservierung-backend";
+
+import { EquipmentControllerApi } from "@/api/raumreservierung-backend";
+import { useApi } from "@/composables/api/useApi.ts";
+import { ApiFactory } from "@/util/apiFactory.ts";
+
+export const useGetAllEquipments = () => {
+  const api = ApiFactory.getInstance(EquipmentControllerApi);
+
+  // eslint-disable-next-line @typescript-eslint/no-invalid-void-type
+  return useApi<void, EquipmentResponseDto[]>(() => api.getAllEquipments());
+};
+
+export const useCreateEquipment = () => {
+  const api = ApiFactory.getInstance(EquipmentControllerApi);
+
+  return useApi<SaveEquipmentRequest, EquipmentResponseDto>((params) =>
+    api.saveEquipment(params)
+  );
+};
+
+export const useUpdateEquipment = () => {
+  const api = ApiFactory.getInstance(EquipmentControllerApi);
+
+  return useApi<UpdateEquipmentRequest, EquipmentResponseDto>((params) =>
+    api.updateEquipment(params)
+  );
+};
+
+export const useDeleteEquipment = () => {
+  const api = ApiFactory.getInstance(EquipmentControllerApi);
+
+  // eslint-disable-next-line @typescript-eslint/no-invalid-void-type
+  return useApi<DeleteEquipmentRequest, void>((params) =>
+    api.deleteEquipment(params)
+  );
+};
