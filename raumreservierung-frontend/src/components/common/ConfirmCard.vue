c6541e344e2ffdf6e4e15ad6d3eb0b96e3071391
langehm <38280183+langehm@users.noreply.github.com>
2026-02-23 15:15:05 +0100

    added generic confirm card

---
+++ raumreservierung-frontend/src/components/common/ConfirmCard.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-card
+    :title="title"
+    :subtitle="subtitle"
+    :text="text"
+  >
+    <template #actions>
+      <slot name="actions">
+        <base-button
+          secondary
+          class="mr-4"
+          @click="emit('no')"
+        >
+          <template #default> {{ t("common.cancel") }} </template>
+          <template #prepend>
+            <v-icon :icon="mdiClose" />
+          </template>
+        </base-button>
+        <base-button @click="emit('yes')">
+          <template #default> {{ t("common.delete") }} </template>
+          <template #append>
+            <v-icon :icon="mdiContentSaveOutline" />
+          </template>
+        </base-button>
+      </slot>
+    </template>
+  </v-card>
+</template>
+
+<script setup lang="ts">
+import { mdiClose, mdiContentSaveOutline } from "@mdi/js";
+import { useI18n } from "vue-i18n";
+
+import BaseButton from "@/components/common/buttons/BaseButton.vue";
+
+const { t } = useI18n();
+
+defineProps<{
+  title: string;
+  subtitle?: string;
+  text?: string;
+  noText?: string;
+  yesText?: string;
+}>();
+
+const emit = defineEmits<{
+  yes: [];
+  no: [];
+}>();
+</script>
+
+<style scoped></style>
