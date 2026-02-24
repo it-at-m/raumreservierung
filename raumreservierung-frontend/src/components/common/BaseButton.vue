6060552799e521ec98342370ffa4cf0dc228348f
flahamue <232542105+flahamue@users.noreply.github.com>
2026-02-24 10:21:20 +0100

    factored out dialog content

---
+++ raumreservierung-frontend/src/components/common/BaseButton.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-btn
+    :color="secondary ? '' : 'secondary'"
+    :variant="secondary ? 'outlined' : 'flat'"
+    :disabled="disabled"
+    @click="emit('click')"
+  >
+    <template #default>
+      <slot name="default" />
+    </template>
+    <template #append>
+      <slot name="append" />
+    </template>
+    <template #prepend>
+      <slot name="prepend" />
+    </template>
+  </v-btn>
+</template>
+
+<script setup lang="ts">
+const { secondary = false } = defineProps<{
+  secondary?: boolean;
+  disabled?: boolean;
+}>();
+
+defineSlots<{
+  default(): void;
+  append(): void;
+  prepend(): void;
+}>();
+
+const emit = defineEmits<(e: "click") => void>();
+</script>
+
+<style scoped></style>
