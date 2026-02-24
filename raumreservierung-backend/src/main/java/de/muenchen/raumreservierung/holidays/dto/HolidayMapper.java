d17862d8c98726eb3c7ecce42a3a1435e8cb2df3
flahamue <232542105+flahamue@users.noreply.github.com>
2026-02-24 10:22:18 +0100

    corrected date format in backend

--- raumreservierung-backend/src/main/java/de/muenchen/raumreservierung/holidays/dto/HolidayMapper.java
+++ raumreservierung-backend/src/main/java/de/muenchen/raumreservierung/holidays/dto/HolidayMapper.java
@@ -3,7 +3,12 @@ package de.muenchen.raumreservierung.holidays.dto;
 import de.muenchen.raumreservierung.holidays.Holiday;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.NullValuePropertyMappingStrategy;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 @Mapper
 public interface HolidayMapper {
@@ -11,5 +16,12 @@ public interface HolidayMapper {
     HolidayResponseDTO toDTO(Holiday h);
 
     @Mapping(target = "id", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "startDate", source = "startDate", qualifiedByName = "dateMapper")
+    @Mapping(target = "endDate", source = "endDate", qualifiedByName = "dateMapper")
     Holiday toEntity(HolidayRequestDTO dto);
+
+    @Named("dateMapper")
+    default LocalDate toLocalDate(LocalDateTime localDateTime) {
+        return localDateTime.atZone(ZoneId.of("UTC")).withZoneSameInstant(ZoneId.of("Europe/Berlin")).toLocalDate();
+    }
 }
