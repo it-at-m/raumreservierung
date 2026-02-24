fe7d03452caebe394bdc39364157f4e089d04ec2
langehm <38280183+langehm@users.noreply.github.com>
2026-02-24 14:30:05 +0100

    added api wrappers

---
+++ raumreservierung-frontend/src/composables/api/useSeatingApi.ts
@@ -0,0 +1,39 @@
+import type {
+  CreateSeatingTypeRequest,
+  DeleteSeatingTypeRequest,
+  SeatingResponseDto,
+  UpdateSeatingTypeRequest,
+} from "@/api/raumreservierung-backend";
+
+import { SeatingControllerApi } from "@/api/raumreservierung-backend";
+import { useApi } from "@/composables/api/useApi.ts";
+import { ApiFactory } from "@/util/apiFactory.ts";
+
+export const useGetAllSeatingTypes = () => {
+  const api = ApiFactory.getInstance(SeatingControllerApi);
+
+  // eslint-disable-next-line @typescript-eslint/no-invalid-void-type
+  return useApi<void, SeatingResponseDto[]>(() => api.getAllSeatingTypes());
+};
+export const useCreateSeatingType = () => {
+  const api = ApiFactory.getInstance(SeatingControllerApi);
+
+  return useApi<CreateSeatingTypeRequest, SeatingResponseDto>((params) =>
+    api.createSeatingType(params)
+  );
+};
+export const useUpdateSeatingType = () => {
+  const api = ApiFactory.getInstance(SeatingControllerApi);
+
+  return useApi<UpdateSeatingTypeRequest, SeatingResponseDto>((params) =>
+    api.updateSeatingType(params)
+  );
+};
+export const usedeleteSeatingType = () => {
+  const api = ApiFactory.getInstance(SeatingControllerApi);
+
+  // eslint-disable-next-line @typescript-eslint/no-invalid-void-type
+  return useApi<DeleteSeatingTypeRequest, void>((params) =>
+    api.deleteSeatingType(params)
+  );
+};
