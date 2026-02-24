da850bad27439946c4de6d8fb95aacb1727cfa49
langehm <38280183+langehm@users.noreply.github.com>
2026-02-24 12:19:04 +0100

    🔧 19 maintenance remove client api from vcs (#20)
    * :fire: removed client code
    
    * :see_no_evil: added client generated apis to gitignore
    
    * :wrench: generated api-spec to dedicated api folder
    
    * :wrench: added client generation to build step - adjusted config
    
    * updated api-spec
    
    * :hammer: added api-gen to lint step
    
    * :sparkles: 17 feature add equipment (#18)
    
    * added simple crud operations to backend
    
    * Introduced dtos
    
    * updated service and controller to use dto
    
    * introduced authority and preauthorized operations in service
    
    * renamed operation
    
    * updated BaseEntity to use serial
    
    * :card_file_box: added schema for equipments and added testdata
    
    * :alien: generated api to add equipment endpoints
    
    * introduced new view for equipment, added to router, added needed privileges, added header and locale
    
    * regenerated api specs and client api
    
    * regenerated api specs and client api
    
    * save
    
    * added basic buttons
    
    * added generic confirm card
    
    * added api composables
    
    * added simple header for views
    
    * added table views
    
    * Changed save into create in api
    
    * :rotating_light: linted  and formated
    
    * :rotating_light: spotless
    
    * adjusted naming
    
    * made dialog width adjustable
    
    * removed comment
    
    * Update raumreservierung-frontend/src/api/raumreservierung-backend/docs/ActuatorApi.md
    
    * Update raumreservierung-frontend/src/api/raumreservierung-backend/docs/TheEntityControllerApi.md
    
    * fixed unused props
    
    * :sparkles: 17 feature add equipment (#18)
    
    * added simple crud operations to backend
    
    * Introduced dtos
    
    * updated service and controller to use dto
    
    * introduced authority and preauthorized operations in service
    
    * renamed operation
    
    * updated BaseEntity to use serial
    
    * :card_file_box: added schema for equipments and added testdata
    
    * :alien: generated api to add equipment endpoints
    
    * introduced new view for equipment, added to router, added needed privileges, added header and locale
    
    * regenerated api specs and client api
    
    * regenerated api specs and client api
    
    * save
    
    * added basic buttons
    
    * added generic confirm card
    
    * added api composables
    
    * added simple header for views
    
    * added table views
    
    * Changed save into create in api
    
    * :rotating_light: linted  and formated
    
    * :rotating_light: spotless
    
    * adjusted naming
    
    * made dialog width adjustable
    
    * removed comment
    
    * Update raumreservierung-frontend/src/api/raumreservierung-backend/docs/ActuatorApi.md
    
    * Update raumreservierung-frontend/src/api/raumreservierung-backend/docs/TheEntityControllerApi.md
    
    * fixed unused props
    
    * removed client api code
    
    * :fire: removed client code
    
    * :see_no_evil: added client generated apis to gitignore
    
    * :wrench: generated api-spec to dedicated api folder
    
    * :wrench: added client generation to build step - adjusted config
    
    * updated api-spec
    
    * :hammer: added api-gen to lint step
    
    * removed client api code
    
    * regenerated api-spec

--- raumreservierung-frontend/src/api/raumreservierung-backend/apis/index.ts
+++
@@ -1,5 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from './ActuatorApi';
-export * from './EquipmentControllerApi';
-export * from './TheEntityControllerApi';
--- raumreservierung-frontend/src/api/raumreservierung-backend/models/index.ts
+++
@@ -1,9 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from './EquipmentRequestDto';
-export * from './EquipmentResponseDto';
-export * from './Link';
-export * from './PageMetadata';
-export * from './PagedModelTheEntityResponseDTO';
-export * from './TheEntityRequestDTO';
-export * from './TheEntityResponseDTO';
