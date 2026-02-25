b12830ca6aa83444ed215b62c73c73772b00e425
langehm <38280183+langehm@users.noreply.github.com>
2026-02-25 10:20:18 +0100

    overhauled confirmCard to allow for more flexibility with content inside

--- raumreservierung-frontend/src/components/common/ConfirmCard.vue
+++ raumreservierung-frontend/src/components/common/ConfirmCard.vue
@@ -2,60 +2,62 @@
   <v-card
     :title="title"
     :subtitle="subtitle"
-    :text="text"
+    :loading="loading"
   >
-    <template #actions>
-      <slot name="actions">
-        <base-button
-          secondary
-          class="mr-4"
-          @click="emit('cancel')"
-        >
-          <template #default> {{ computedCancelText }} </template>
-          <template #prepend>
-            <v-icon :icon="mdiClose" />
-          </template>
-        </base-button>
-        <base-button @click="emit('confirm')">
-          <template #default> {{ computedConfirmText }} </template>
-          <template #append>
-            <v-icon :icon="mdiContentSaveOutline" />
-          </template>
-        </base-button>
+    <template #text>
+      <slot name="text">
+        {{ text }}
       </slot>
+    </template>
+    <template #actions>
+      <div class="mb-4 mr-4">
+        <slot
+          name="cancel"
+          :props="{ onClick: cancel }"
+        >
+          <base-button
+            secondary
+            class="mr-4"
+            @click="cancel"
+            :prepend-icon="mdiClose"
+            :text="t('common.cancel')"
+          />
+        </slot>
+        <slot
+          name="confirm"
+          :props="{ onClick: confirm }"
+        />
+      </div>
     </template>
   </v-card>
 </template>
 
 <script setup lang="ts">
-import { mdiClose, mdiContentSaveOutline } from "@mdi/js";
-import { computed } from "vue";
+import { mdiClose } from "@mdi/js";
 import { useI18n } from "vue-i18n";
 
 import BaseButton from "@/components/common/buttons/BaseButton.vue";
 
 const { t } = useI18n();
 
-const { confirmText, cancelText } = defineProps<{
+const { loading = false } = defineProps<{
   title: string;
   subtitle?: string;
   text?: string;
-  confirmText?: string;
-  cancelText?: string;
+  loading?: boolean;
 }>();
-
-const computedConfirmText = computed(() =>
-  confirmText ? confirmText : t("common.delete")
-);
-
-const computedCancelText = computed(() =>
-  cancelText ? cancelText : t("common.cancel")
-);
 
 const emit = defineEmits<{
   confirm: [];
   cancel: [];
 }>();
+
+const confirm = () => {
+  console.log("confirmed");
+  emit("confirm");
+};
+
+const cancel = () => emit("cancel");
 </script>
 
 <style scoped></style>
