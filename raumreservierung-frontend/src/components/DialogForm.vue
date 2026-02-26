84fe889c539743d9e929386458ff31b0b813a8f0
flahamue <232542105+flahamue@users.noreply.github.com>
2026-02-26 13:14:29 +0100

    added toggle between public holiday and school holiday view

--- raumreservierung-frontend/src/components/DialogForm.vue
+++ raumreservierung-frontend/src/components/DialogForm.vue
@@ -1,3 +1,11 @@
+<i18n>
+{
+  "en": {
+    "\"t('domain.holidays.public.date')\"": "\"t('domain.holidays.public.date')\""
+  }
+}
+</i18n>
+
 <script setup lang="ts">
 import type { HolidayRequestDTO } from "@/api/raumreservierung-backend";
 
@@ -11,7 +19,10 @@ const emit = defineEmits<{
   close: [];
 }>();
 const modelValue = defineModel<HolidayRequestDTO>({ required: true });
-const { loading } = defineProps<{ loading: boolean }>();
+const { loading, isPublic } = defineProps<{
+  loading: boolean;
+  isPublic: boolean;
+}>();
 
 const { t } = useI18n();
 </script>
@@ -31,12 +42,43 @@ const { t } = useI18n();
         :label="t('domain.holidays.public.name')"
         variant="outlined"
       ></v-text-field>
-      <v-date-input
-        v-model="modelValue.startDate"
-        :label="t('domain.holidays.public.date')"
-        :prepend-icon="mdiCalendar"
+      <v-card
         variant="outlined"
-      ></v-date-input>
+        persistent-placeholder
+      >
+        <template #title>
+          <div class="v-label font-weight-regular">
+            {{ t("domain.holidays.public.date") }}
+          </div>
+        </template>
+        <v-row class="px-2">
+          <v-col
+            cols="12"
+            :sm="isPublic ? 12 : 6"
+          >
+            <v-date-input
+              v-model="modelValue.startDate"
+              :label="
+                isPublic ? undefined : t('domain.holidays.school.startDate')
+              "
+              :prepend-icon="mdiCalendar"
+              variant="outlined"
+            ></v-date-input>
+          </v-col>
+          <v-col
+            cols="12"
+            sm="6"
+            v-if="!isPublic"
+          >
+            <v-date-input
+              v-model="modelValue.endDate"
+              :label="t('domain.holidays.school.endDate')"
+              :prepend-icon="mdiCalendar"
+              variant="outlined"
+            ></v-date-input>
+          </v-col>
+        </v-row>
+      </v-card>
     </v-card-text>
     <v-card-actions>
       <v-spacer></v-spacer>
