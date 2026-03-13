3398f8c07a7974e8f0176a23aa7a08310cde2dda
langehm <38280183+langehm@users.noreply.github.com>
2026-03-13 13:41:25 +0100

    added base view and crud card for displaying custom tables

---
+++ raumreservierung-frontend/src/components/common/CrudCard.vue
@@ -0,0 +1,176 @@
+<template>
+  <div>
+    <v-dialog
+      :model-value="showDialog"
+      width="90%"
+      :max-width="maxDialogWidth"
+      persistent
+      close-on-back
+    >
+      <confirm-card
+        v-if="dialogMode == 'form'"
+        :loading="loading"
+        :title="
+          t(activeItem.id ? 'generics.edit' : 'generics.create', {
+            domain: domain,
+          })
+        "
+        @confirm="handleSave"
+        @cancel="closeDialog"
+      >
+        <template #text>
+          <slot
+            name="form"
+            :item="activeItem"
+            :updateItem="updateActiveItem"
+            :updateValidity="updateFormValidity"
+          />
+        </template>
+        <template #confirm="{ props }">
+          <base-button
+            :text="t('common.save')"
+            :append-icon="mdiContentSaveOutline"
+            :disabled="!isFormSlotValid"
+            v-bind="props"
+          />
+        </template>
+      </confirm-card>
+
+      <confirm-card
+        v-else-if="dialogMode === 'delete'"
+        :title="t('generics.delete', { domain })"
+        :text="t('generics.confirmDelete', { domain })"
+        :loading="loading"
+        @cancel="closeDialog"
+        @confirm="executeDelete"
+      >
+        <template #confirm="{ props }">
+          <base-button
+            :text="t('common.delete')"
+            :append-icon="mdiTrashCanOutline"
+            v-bind="props"
+          />
+        </template>
+      </confirm-card>
+    </v-dialog>
+
+    <v-card>
+      <template #title>
+        <v-row align-content="center">
+          <v-col class="d-flex align-center justify-end">
+            <slot
+              name="tableActions"
+              :openCreate="openCreate"
+            >
+              <base-button
+                @click="openCreate"
+                :append-icon="mdiPlus"
+                :text="t('common.add')"
+              />
+            </slot>
+          </v-col>
+        </v-row>
+        <v-row class="mt-2">
+          <v-col>
+            <v-divider />
+          </v-col>
+        </v-row>
+      </template>
+      <template #text>
+        <slot
+          name="table"
+          :openEdit="openEdit"
+          :openDelete="promptDelete"
+        />
+      </template>
+    </v-card>
+  </div>
+</template>
+
+<script setup lang="ts" generic="T extends { id?: string }">
+import { mdiContentSaveOutline, mdiPlus, mdiTrashCanOutline } from "@mdi/js";
+import { computed, ref } from "vue";
+import { useI18n } from "vue-i18n";
+
+import BaseButton from "@/components/common/buttons/BaseButton.vue";
+import ConfirmCard from "@/components/common/ConfirmCard.vue";
+
+const { t } = useI18n();
+
+type DialogMode = "form" | "delete" | null;
+const dialogMode = ref<DialogMode>(null);
+const showDialog = computed(() => dialogMode.value !== null);
+
+const {
+  maxDialogWidth = "800px",
+  emptyItemTemplate,
+  loading = false,
+} = defineProps<{
+  maxDialogWidth?: string;
+  emptyItemTemplate: T;
+  domain: string;
+  loading?: boolean;
+}>();
+
+const activeItem = ref<T>({ ...emptyItemTemplate } as T);
+
+const isFormSlotValid = ref(false);
+
+const emit = defineEmits<{
+  create: [item: T];
+  update: [item: T];
+  delete: [id: string];
+  // Sadly there is no type for the emit of updatedOptions ...
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  "update:options": [options: any];
+}>();
+
+// --- Functions ---
+const updateFormValidity = (valid: boolean | null) => {
+  isFormSlotValid.value = !!valid;
+};
+
+const updateActiveItem = (newValue: T) => {
+  activeItem.value = newValue;
+};
+
+const openCreate = () => {
+  activeItem.value = { ...emptyItemTemplate } as T;
+  dialogMode.value = "form";
+};
+
+const openEdit = (item: T) => {
+  activeItem.value = JSON.parse(JSON.stringify(item)) as T;
+  dialogMode.value = "form";
+};
+
+const promptDelete = (item: T) => {
+  activeItem.value = item;
+  dialogMode.value = "delete";
+};
+
+const handleSave = () => {
+  if (activeItem.value.id) {
+    emit("update", activeItem.value);
+  } else {
+    emit("create", activeItem.value);
+  }
+};
+
+const executeDelete = () => {
+  if (activeItem.value?.id) {
+    emit("delete", activeItem.value.id);
+  }
+};
+
+const closeDialog = () => {
+  dialogMode.value = null;
+  activeItem.value = { ...emptyItemTemplate } as T;
+};
+
+defineExpose({
+  closeDialog,
+});
+</script>
+
+<style scoped></style>
