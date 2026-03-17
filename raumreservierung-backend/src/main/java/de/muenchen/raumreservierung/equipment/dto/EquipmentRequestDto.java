cfdba09a6b4ef648a457c1b5676994e8587af630
langehm <38280183+langehm@users.noreply.github.com>
2026-03-17 15:56:40 +0100

    added isActive to dtos and regenerated api

--- raumreservierung-backend/src/main/java/de/muenchen/raumreservierung/equipment/dto/EquipmentRequestDto.java
+++ raumreservierung-backend/src/main/java/de/muenchen/raumreservierung/equipment/dto/EquipmentRequestDto.java
@@ -3,5 +3,6 @@ package de.muenchen.raumreservierung.equipment.dto;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
-public record EquipmentRequestDto(@NotNull @Size(min = 2, max = 100) String name, String description) {
+public record EquipmentRequestDto(@NotNull @Size(min = 2, max = 100) String name, String description,
+                                  boolean isActive) {
 }
