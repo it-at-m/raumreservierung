6404b1720997aec0c7ce9a0bd38103d45f79bf64
langehm <38280183+langehm@users.noreply.github.com>
2026-03-17 15:56:40 +0100

    added isActive to dtos and regenerated api

--- raumreservierung-backend/src/main/java/de/muenchen/raumreservierung/equipment/dto/EquipmentResponseDto.java
+++ raumreservierung-backend/src/main/java/de/muenchen/raumreservierung/equipment/dto/EquipmentResponseDto.java
@@ -2,7 +2,9 @@ package de.muenchen.raumreservierung.equipment.dto;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+
 import java.util.UUID;
 
-public record EquipmentResponseDto(UUID id, @NotNull @Size(min = 2, max = 100) String name, @NotNull @Size(max = 255) String description) {
+public record EquipmentResponseDto(UUID id, @NotNull @Size(min = 2, max = 100) String name,
+                                   @NotNull @Size(max = 255) String description, boolean isActive) {
 }
--- raumreservierung-backend/src/main/java/de/muenchen/raumreservierung/seating/dto/SeatingTypeResponseDto.java
+++ raumreservierung-backend/src/main/java/de/muenchen/raumreservierung/seating/dto/SeatingTypeResponseDto.java
@@ -2,7 +2,9 @@ package de.muenchen.raumreservierung.seating.dto;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+
 import java.util.UUID;
 
-public record SeatingTypeResponseDto(UUID id, @NotNull @Size(min = 2, max = 100) String name, @NotNull @Size(max = 255) String description) {
+public record SeatingTypeResponseDto(UUID id, @NotNull @Size(min = 2, max = 100) String name,
+                                     @NotNull @Size(max = 255) String description, boolean isActive) {
 }
