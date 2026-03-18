e48584e1da3f223880e1dfadbbdcc1004ff47b5f
langehm <38280183+langehm@users.noreply.github.com>
2026-03-18 17:53:48 +0100

    added details view of room

---
+++ raumreservierung-backend/src/main/java/de/muenchen/raumreservierung/room/dto/RoomDetailsResponseDTO.java
@@ -0,0 +1,21 @@
+package de.muenchen.raumreservierung.room.dto;
+
+import de.muenchen.raumreservierung.equipment.dto.EquipmentResponseDto;
+import de.muenchen.raumreservierung.seating.dto.SeatingTypeResponseDto;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.util.Set;
+import java.util.UUID;
+
+public record RoomDetailsResponseDTO(
+        UUID id,
+        @NotNull @Size(min = 2, max = 100) String name,
+        @NotNull @Size(max = 10) String number,
+        @NotNull @Size(max = 255) String address,
+        @NotNull int capacity, @NotNull @Size(max = 1000) String information,
+        @NotNull @Size(max = 1000) String note, @NotNull Boolean availability,
+        @NotNull int area, @NotNull Set<SeatingTypeResponseDto> seatingType,
+        @NotNull Set<EquipmentResponseDto> equipment
+) {
+}
