91cb6bccc19b1f35ad1f8b7949be15467121408a
langehm <38280183+langehm@users.noreply.github.com>
2026-03-19 16:45:59 +0100

    customized actions to be disabled upon active items

--- raumreservierung-frontend/src/views/SeatingTypeView.vue
+++ raumreservierung-frontend/src/views/SeatingTypeView.vue
@@ -26,6 +26,32 @@
         class="pointer-events-none"
       />
     </template>
+    <template #itemActions="{ openEdit, promptDelete, item }">
+      <v-row align-content="center">
+        <v-col
+          class="pa-0"
+          cols="12"
+          sm="6"
+        >
+          <action-button
+            type="edit"
+            class="mr-1"
+            @click="openEdit(item)"
+          />
+        </v-col>
+        <v-col
+          class="pa-0"
+          cols="12"
+          sm="6"
+        >
+          <action-button
+            :disabled="item.isActive"
+            type="delete"
+            @click="promptDelete(item)"
+          />
+        </v-col>
+      </v-row>
+    </template>
   </generic-table-crud-view>
 </template>
 
@@ -37,6 +63,7 @@ import { onMounted, useTemplateRef } from "vue";
 import { useI18n } from "vue-i18n";
 
 import { Levels } from "@/api/error.ts";
+import ActionButton from "@/components/common/buttons/ActionButton.vue";
 import GenericTableCrudView from "@/components/common/GenericTableCrudView.vue";
 import SeatingTypeForm from "@/components/SeatingTypeForm.vue";
 import {
