f3bea52500c880dbf722673452781b634aaf5d8d
langehm <38280183+langehm@users.noreply.github.com>
2026-03-19 09:37:12 +0100

    :poop: added new methods for put and post 2

--- raumreservierung-backend/src/main/java/de/muenchen/raumreservierung/room/dto/RoomRequestDTO.java
+++ raumreservierung-backend/src/main/java/de/muenchen/raumreservierung/room/dto/RoomRequestDTO.java
@@ -2,15 +2,12 @@ package de.muenchen.raumreservierung.room.dto;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+
 import java.util.Set;
 import java.util.UUID;
 
 public record RoomRequestDTO(@NotNull @Size(min = 2, max = 100) String name, @Size(max = 10) String number,
-        @Size(max = 255) String address, int capacity, @Size(max = 1000) String information,
-        @Size(max = 1000) String note, Boolean availability, int area, Set<UUID> seatingTypeIds,
-        Set<UUID> equipmentIds) {
-    public RoomRequestDTO {
-        seatingTypeIds = seatingTypeIds == null ? Set.of() : Set.copyOf(seatingTypeIds);
-        equipmentIds = equipmentIds == null ? Set.of() : Set.copyOf(equipmentIds);
-    }
+                             @Size(max = 255) String address, int capacity, @Size(max = 1000) String information,
+                             @Size(max = 1000) String note, Boolean availability, int area, Set<UUID> seatingTypeIds,
+                             Set<UUID> equipmentIds) {
 }
