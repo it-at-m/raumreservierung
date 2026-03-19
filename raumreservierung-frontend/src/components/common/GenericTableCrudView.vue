77aee42152800656ee2b12e091a9d919f14c6d8f
langehm <38280183+langehm@users.noreply.github.com>
2026-03-19 17:41:06 +0100

    typed set the itemAction slot and generic slot and form slot

--- raumreservierung-frontend/src/components/common/GenericTableCrudView.vue
+++ raumreservierung-frontend/src/components/common/GenericTableCrudView.vue
@@ -25,6 +25,7 @@
         @cancel="closeDialog"
       >
         <template #text>
+          <!-- @vue-ignore -->
           <slot
             name="form"
             :item="activeItem"
@@ -124,6 +125,7 @@
 
 <script setup lang="ts" generic="T extends { id?: string }">
 import type { TableHeader } from "@/components/common/CardTable.vue";
+import type { Slot, VNode } from "vue";
 
 import { mdiContentSaveOutline, mdiPlus, mdiTrashCanOutline } from "@mdi/js";
 import { computed, ref } from "vue";
@@ -213,6 +215,20 @@ const closeDialog = () => {
 defineExpose({
   closeDialog,
 });
+
+defineSlots<{
+  form(props: {
+    item: T;
+    updateItem: (item: T) => void;
+    updateValidity: (item: boolean | null) => void;
+  }): VNode[];
+  itemActions(props: {
+    item: T;
+    openEdit: (item: T) => void;
+    promptDelete: (item: T) => void;
+  }): VNode[];
+  [key: string]: Slot;
+}>();
 </script>
 
 <style scoped></style>
