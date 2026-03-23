a1599fe9ecc6118e66c8166aeaad526d2e273134
langehm <38280183+langehm@users.noreply.github.com>
2026-03-23 10:28:39 +0100

    fixed duplicate route from merging

--- raumreservierung-frontend/src/App.vue
+++ raumreservierung-frontend/src/App.vue
@@ -81,6 +81,4 @@ const appswitcherBaseUrl = APPSWITCHER_URL;
 
 const [drawer, toggleDrawer] = useToggle(true);
 const isSmallScreen = useMediaQuery("(max-width: 1280px)");
-
-const route = useRoute();
 </script>
