f94ac59e56dabf240f5244c58c71a8bc23eb7fc2
Flo <232542105+flahamue@users.noreply.github.com>
2026-05-12 17:10:47 +0200

    :bug: 113 bug wrong message on person snackbar (#114)
    * fixed snackbar display text for persons
    
    * updated outdated authorities
    
    * fixed linter findings
    
    * fixed spotless findings
    
    * fixed trivy findings
    
    * fixed bug when trying to change holidays
    
    * fixed bug when trying to create holidays
    
    * fix with eslint
    
    * updated pom.xml to fix CVEs
    
    * dates now copied with structuredClone
    
    ---------
    
    Co-authored-by: langehm <38280183+langehm@users.noreply.github.com>

--- raumreservierung-frontend/src/components/HolidayForm.vue
+++ raumreservierung-frontend/src/components/HolidayForm.vue
@@ -8,41 +8,41 @@
       v-model="modelValue.name"
       :label="t('domain.holidays.public.name')"
       :rules="[maxNameLength, minTwoChars]"
-      variant="outlined"
       autofocus
+      variant="outlined"
     />
     <card-form :subtitle="t('domain.holidays.public.date')">
       <template #text>
         <v-row justify="start">
           <v-col
+            class="pr-10 pb-0"
             cols="12"
             sm="6"
-            class="pr-10 pb-0"
           >
             <v-date-input
               ref="startDateInput"
               v-model="modelValue.startDate"
-              :label="isPublic ? '' : t('domain.holidays.school.startDate')"
-              :rules="[datePicked, dateRules('startDate')]"
               :allowed-dates="isDateAllowed('startDate')"
+              :label="isPublic ? '' : t('domain.holidays.school.startDate')"
               :prepend-icon="mdiCalendar"
+              :rules="[datePicked, dateRules('startDate')]"
               variant="underlined"
               @update:model-value="validateDate('endDate')"
             />
           </v-col>
           <v-col
             v-if="!isPublic"
+            class="pr-10 pb-0"
             cols="12"
             sm="6"
-            class="pr-10 pb-0"
           >
             <v-date-input
               ref="endDateInput"
               v-model="modelValue.endDate"
-              :label="t('domain.holidays.school.endDate')"
-              :rules="[datePicked, dateRules('endDate')]"
               :allowed-dates="isDateAllowed('endDate')"
+              :label="t('domain.holidays.school.endDate')"
               :prepend-icon="mdiCalendar"
+              :rules="[datePicked, dateRules('endDate')]"
               variant="underlined"
               @update:model-value="validateDate('startDate')"
             />
