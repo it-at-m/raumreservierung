d842e2fdb94f70df01b98448a83e32c03004c341
flahamue <232542105+flahamue@users.noreply.github.com>
2026-05-18 15:13:59 +0200

    Validation of ScheduleTemplate

--- raumreservierung-backend/src/main/java/de/muenchen/raumreservierung/booking/ScheduleTemplate.java
+++ raumreservierung-backend/src/main/java/de/muenchen/raumreservierung/booking/ScheduleTemplate.java
@@ -1,17 +1,35 @@
 package de.muenchen.raumreservierung.booking;
 
+import de.muenchen.raumreservierung.common.BadRequestException;
 import jakarta.persistence.Embeddable;
-import java.io.Serial;
+import jakarta.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Embeddable
 public record ScheduleTemplate(
-        LocalDateTime occupancyStart,
-        LocalDateTime occupancyEnd,
+        @NotNull LocalDateTime occupancyStart,
+        @NotNull LocalDateTime occupancyEnd,
         LocalDateTime appointmentStart,
         LocalDateTime appointmentEnd) implements Serializable {
 
-    @Serial
-    private static final long serialVersionUID = 1L;
+    private static final String ERROR_OCCUPANCY_END_BEFORE_START = "occupancyEnd cannot be before occupancyStart";
+
+    public ScheduleTemplate {
+        Objects.requireNonNull(occupancyStart);
+        Objects.requireNonNull(occupancyEnd);
+
+        if (occupancyEnd.isBefore(occupancyStart)) {
+            throw new BadRequestException(ERROR_OCCUPANCY_END_BEFORE_START);
+        }
+
+        if (appointmentStart == null || appointmentStart.isBefore(occupancyStart) || appointmentStart.isAfter(occupancyEnd)) {
+            appointmentStart = occupancyStart;
+        }
+
+        if (appointmentEnd == null || appointmentEnd.isAfter(occupancyEnd) || appointmentEnd.isBefore(occupancyStart)) {
+            appointmentEnd = occupancyEnd;
+        }
+    }
 }
