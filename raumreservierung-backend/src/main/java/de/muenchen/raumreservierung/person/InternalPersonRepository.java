76323d56068c00b446581a7c68c96feac95ac6f9
flahamue <232542105+flahamue@users.noreply.github.com>
2026-05-18 15:13:08 +0200

    fixed linter findings

--- raumreservierung-backend/src/main/java/de/muenchen/raumreservierung/person/InternalPersonRepository.java
+++ raumreservierung-backend/src/main/java/de/muenchen/raumreservierung/person/InternalPersonRepository.java
@@ -1,12 +1,11 @@
 package de.muenchen.raumreservierung.person;
 
 import de.muenchen.raumreservierung.person.domain.InternalPerson;
+import java.util.Optional;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
-import java.util.UUID;
 
 @Repository
 public interface InternalPersonRepository extends JpaRepository<InternalPerson, UUID>, JpaSpecificationExecutor<InternalPerson> {
