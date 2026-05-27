0dabcd0d6fefdab119bec0fc9105ba49dd04292e
flahamue <232542105+flahamue@users.noreply.github.com>
2026-05-27 10:19:27 +0200

    removed unnecessary override

--- raumreservierung-backend/src/main/java/de/muenchen/raumreservierung/appointment/AppointmentRepository.java
+++ raumreservierung-backend/src/main/java/de/muenchen/raumreservierung/appointment/AppointmentRepository.java
@@ -1,15 +1,9 @@
 package de.muenchen.raumreservierung.appointment;
 
 import java.util.UUID;
-import lombok.NonNull;
-import org.jspecify.annotations.Nullable;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface AppointmentRepository extends JpaRepository<Appointment, UUID>, JpaSpecificationExecutor<Appointment> {
-    @Override
-    @NonNull Page<Appointment> findAll(@Nullable Specification<Appointment> specification, @NonNull Pageable pageable);
+
 }
