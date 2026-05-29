596247f811fb2cec27d64318387e509bed7e89f9
langehm <38280183+langehm@users.noreply.github.com>
2026-05-29 16:57:13 +0200

    component for display of appointment

--- raumreservierung-frontend/src/components/booking/AppointmentListItem.vue
+++ raumreservierung-frontend/src/components/booking/AppointmentListItem.vue
@@ -1,46 +1,107 @@
 <template>
-  <v-list-item>
+  <v-list-item
+    :lines="!occupancyTimeDiffers && !appointmentTimeDiffers ? false : 'two'"
+    rounded
+    :class="{
+      'py-0 mb-n0': !occupancyTimeDiffers && !appointmentTimeDiffers,
+    }"
+  >
     <template #title>
-      <span>
-        {{
-          useDateFormat(appointment.schedule.occupancyStart, DATE_FORMAT_DDMMYY)
-        }}
-      </span>
-      <span v-if="isMultiDay">
-        -
-        {{
-          useDateFormat(appointment.schedule.occupancyEnd, DATE_FORMAT_DDMMYY)
-        }}
-      </span>
+      <v-row
+        style="max-width: 350px"
+        no-gutters
+        class="align-center mb-1"
+      >
+        <v-col cols="8">
+          {{
+            useDateFormat(
+              appointment.schedule.occupancyStart,
+              DATE_FORMAT_DDMMYY
+            ).value
+          }}
+          <span v-if="isMultiDay">
+            -
+            {{
+              useDateFormat(
+                appointment.schedule.occupancyEnd,
+                DATE_FORMAT_DDMMYY
+              ).value
+            }}
+          </span>
+        </v-col>
+        <v-col
+          cols="4"
+          class="text-center"
+        >
+          <v-chip
+            v-if="occupancyTimeDiffers || appointmentTimeDiffers"
+            color="warning"
+            size="x-small"
+            variant="flat"
+          >
+            Abweichende Zeit
+          </v-chip>
+        </v-col>
+      </v-row>
     </template>
+
     <template #subtitle>
-      <v-list-item-subtitle v-if="occupancyTimeDiffers">
-        <span>OccupancyTime: </span>
-        <span
-          >{{
+      <v-row
+        v-if="occupancyTimeDiffers"
+        style="max-width: 350px"
+        no-gutters
+        class="align-center"
+      >
+        <v-col
+          cols="8"
+          class="text-medium-emphasis"
+        >
+          Belegungszeit:
+        </v-col>
+        <v-col
+          cols="4"
+          class="text-high-emphasis text-center"
+        >
+          {{
             useDateFormat(appointment.schedule.occupancyStart, TIME_FORMAT_HHMM)
+              .value
           }}
           -
           {{
             useDateFormat(appointment.schedule.occupancyEnd, TIME_FORMAT_HHMM)
-          }}</span
+              .value
+          }}
+        </v-col>
+      </v-row>
+      <v-row
+        v-if="appointmentTimeDiffers"
+        style="max-width: 350px"
+        no-gutters
+        class="align-center mt-1"
+      >
+        <v-col
+          cols="8"
+          class="text-medium-emphasis"
         >
-      </v-list-item-subtitle>
-      <v-list-item-subtitle v-if="appointmentTimeDiffers">
-        <span>AppointmentTime: </span>
-        <span
-          >{{
+          Veranstaltungszeit:
+        </v-col>
+        <v-col
+          cols="4"
+          class="text-high-emphasis text-center"
+        >
+          {{
             useDateFormat(
               appointment.schedule.appointmentStart,
               TIME_FORMAT_HHMM
-            )
+            ).value
           }}
           -
           {{
             useDateFormat(appointment.schedule.appointmentEnd, TIME_FORMAT_HHMM)
-          }}</span
-        >
-      </v-list-item-subtitle>
+              .value
+          }}
+        </v-col>
+      </v-row>
     </template>
   </v-list-item>
 </template>
@@ -55,6 +116,7 @@ import { useDateFormat } from "@vueuse/core";
 import { computed } from "vue";
 
 import { DATE_FORMAT_DDMMYY, TIME_FORMAT_HHMM } from "@/constants.ts";
+import { dateEquals, timeEquals } from "@/util/timeUtil.ts";
 
 const { appointment, schedule } = defineProps<{
   appointment: AppointmentMinimalResponseDTO;
@@ -63,36 +125,25 @@ const { appointment, schedule } = defineProps<{
 
 const occupancyTimeDiffers = computed(
   () =>
-    appointment.schedule.occupancyStart?.getMinutes() !==
-      schedule.occupancyStart?.getMinutes() ||
-    appointment.schedule.occupancyEnd?.getMinutes() !==
-      schedule.occupancyEnd?.getMinutes() ||
-    appointment.schedule.occupancyStart?.getHours() !==
-      schedule.occupancyStart?.getHours() ||
-    appointment.schedule.occupancyEnd?.getHours() !==
-      schedule.occupancyEnd?.getHours()
+    !timeEquals(appointment.schedule.occupancyStart, schedule.occupancyStart) ||
+    !timeEquals(appointment.schedule.occupancyEnd, schedule.occupancyEnd)
 );
 
 const appointmentTimeDiffers = computed(
   () =>
-    appointment.schedule.appointmentStart?.getMinutes() !==
-      schedule.appointmentStart?.getMinutes() ||
-    appointment.schedule.appointmentEnd?.getMinutes() !==
-      schedule.appointmentEnd?.getMinutes() ||
-    appointment.schedule.appointmentStart?.getHours() !==
-      schedule.appointmentStart?.getHours() ||
-    appointment.schedule.appointmentEnd?.getHours() !==
-      schedule.appointmentEnd?.getHours()
+    !timeEquals(
+      appointment.schedule.appointmentStart,
+      schedule.appointmentStart
+    ) ||
+    !timeEquals(appointment.schedule.appointmentEnd, schedule.appointmentEnd)
 );
 
 const isMultiDay = computed(
   () =>
-    appointment.schedule.occupancyStart.getDate() !==
-      appointment.schedule.occupancyEnd.getDate() ||
-    appointment.schedule.occupancyStart.getMonth() !==
-      appointment.schedule.occupancyEnd.getMonth() ||
-    appointment.schedule.occupancyStart.getFullYear() !==
-      appointment.schedule.occupancyEnd.getFullYear()
+    !dateEquals(
+      appointment.schedule.occupancyStart,
+      appointment.schedule.occupancyEnd
+    )
 );
 </script>
 
