6d305af8670650b789a9c6d0abec2308326bc733
langehm <38280183+langehm@users.noreply.github.com>
2026-06-12 14:31:13 +0200

    fixed missing display of active in roomdetails

--- raumreservierung-frontend/src/views/rooms/RoomsDetailsView.vue
+++ raumreservierung-frontend/src/views/rooms/RoomsDetailsView.vue
@@ -42,12 +42,37 @@
             type="article"
           >
             <v-responsive>
-              <v-card
-                flat
-                :title="roomData?.name"
-                :subtitle="`${roomData?.number} - ${roomData?.location}`"
-                :text="roomData?.locationDescription"
-              />
+              <v-card flat>
+                <v-card-item>
+                  <v-card-title>{{ roomData?.name }}</v-card-title>
+
+                  <v-card-subtitle class="opacity-100">
+                    <span class="text-medium-emphasis">
+                      {{
+                        t("common.format.dateRange", {
+                          start: roomData?.number,
+                          end: roomData?.location,
+                        })
+                      }}
+                    </span>
+
+                    <v-chip
+                      v-if="!roomData?.isActive"
+                      class="ml-2"
+                      color="primary"
+                      :text="
+                        t('generics.inActive', {
+                          domain: t('domain.room.header'),
+                        })
+                      "
+                    />
+                  </v-card-subtitle>
+                </v-card-item>
+
+                <v-card-text>
+                  {{ roomData?.locationDescription }}
+                </v-card-text>
+              </v-card>
             </v-responsive>
           </v-skeleton-loader>
         </v-col>
