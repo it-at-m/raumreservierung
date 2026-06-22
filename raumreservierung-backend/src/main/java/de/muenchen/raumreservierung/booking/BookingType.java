065c54dc43dc5bfd1c21c539711a4eb2276df3a9
flahamue <232542105+flahamue@users.noreply.github.com>
2026-06-22 09:29:48 +0200

    removed properties

--- raumreservierung-backend/src/main/java/de/muenchen/raumreservierung/booking/BookingType.java
+++ raumreservierung-backend/src/main/java/de/muenchen/raumreservierung/booking/BookingType.java
@@ -1,16 +1,7 @@
 package de.muenchen.raumreservierung.booking;
 
-import lombok.Getter;
-
-@Getter
 public enum BookingType {
-    FREE(false),
-    SERVICE(true),
-    NORMAL(true);
-
-    private final boolean blocking;
-
-    BookingType(final boolean blocking) {
-        this.blocking = blocking;
-    }
+    FREE,
+    SERVICE,
+    NORMAL
 }
