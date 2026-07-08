fc7fc24a2b93fbbaf366b40f2bce91b8a00cc4c4
flahamue <232542105+flahamue@users.noreply.github.com>
2026-07-08 11:50:28 +0200

    spotless fix

--- raumreservierung-backend/src/test/java/de/muenchen/raumreservierung/booking/BookingStatusTest.java
+++ raumreservierung-backend/src/test/java/de/muenchen/raumreservierung/booking/BookingStatusTest.java
@@ -9,6 +9,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import de.muenchen.raumreservierung.TestConstants;
 import de.muenchen.raumreservierung.booking.dto.BookingDetailResponseDTO;
