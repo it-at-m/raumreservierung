f009ac9b72033f6aac28cfbc3ccb44e916e7a43f
flahamue <232542105+flahamue@users.noreply.github.com>
2026-07-31 10:03:55 +0200

    fixed parameter pollution

--- raumreservierung-backend/src/main/java/de/muenchen/raumreservierung/booking/dto/BookingFilterDTO.java
+++ raumreservierung-backend/src/main/java/de/muenchen/raumreservierung/booking/dto/BookingFilterDTO.java
@@ -2,6 +2,11 @@ package de.muenchen.raumreservierung.booking.dto;
 
 import de.muenchen.raumreservierung.booking.BookingStatus;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.Explode;
+import io.swagger.v3.oas.annotations.enums.ParameterStyle;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.UUID;
@@ -11,5 +16,8 @@ public record BookingFilterDTO(
         UUID roomId,
         OffsetDateTime start,
         OffsetDateTime end,
-        List<BookingStatus> status) {
+        @Parameter(
+                style = ParameterStyle.FORM, explode = Explode.FALSE,
+                array = @ArraySchema(schema = @Schema(implementation = BookingStatus.class))
+        ) List<BookingStatus> status) {
 }
--- raumreservierung-backend/src/main/java/de/muenchen/raumreservierung/configuration/OpenAPIDocumentationConfiguration.java
+++ raumreservierung-backend/src/main/java/de/muenchen/raumreservierung/configuration/OpenAPIDocumentationConfiguration.java
@@ -2,9 +2,7 @@ package de.muenchen.raumreservierung.configuration;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.parameters.Parameter;
 import lombok.RequiredArgsConstructor;
-import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,23 +16,5 @@ public class OpenAPIDocumentationConfiguration {
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info().title(openAPIProperties.getName()).description(openAPIProperties.getDescription()).version(openAPIProperties.getVersion()));
-    }
-
-    @Bean
-    public OpenApiCustomizer customizeBookingStatusParameter() {
-        return openApi -> {
-            if (openApi.getPaths() != null) {
-                openApi.getPaths().values().forEach(pathItem -> {
-                    if (pathItem.getGet() != null && pathItem.getGet().getParameters() != null) {
-                        pathItem.getGet().getParameters().stream()
-                                .filter(parameter -> "status".equals(parameter.getName()) && "query".equals(parameter.getIn()))
-                                .forEach(parameter -> {
-                                    parameter.setStyle(Parameter.StyleEnum.FORM);
-                                    parameter.setExplode(Boolean.FALSE);
-                                });
-                    }
-                });
-            }
-        };
     }
 }
