a4ac25eb3b41ee5a44fa76413c7c71a69bbaf870
flahamue <232542105+flahamue@users.noreply.github.com>
2026-08-06 14:44:06 +0200

    reverted to last running version and added unique constraint separately

--- raumreservierung-backend/src/main/resources/db/migration/schema/V007__Room_schema.sql
+++ raumreservierung-backend/src/main/resources/db/migration/schema/V007__Room_schema.sql
@@ -9,7 +9,6 @@ CREATE TABLE room
     is_active            BOOLEAN DEFAULT TRUE,
     area                 INT,
     contact_person_id    UUID,
-    picture              bytea,
 
     PRIMARY KEY (id)
 );
@@ -32,6 +31,5 @@ CREATE TABLE room_seating_capacity
     room_id         UUID    NOT NULL,
 
     PRIMARY KEY (id),
-    FOREIGN KEY (room_id) REFERENCES room (id) ON DELETE CASCADE,
-    CONSTRAINT uq_room_seating_type UNIQUE (room_id, seating_type_id)
+    FOREIGN KEY (room_id) REFERENCES room (id) ON DELETE CASCADE
 );
