de7d9da78fbb5db8812a2de5bd347c94df8d2207
langehm <38280183+langehm@users.noreply.github.com>
2026-08-11 11:58:47 +0200

    :sparkles: added room selection to view and added the ability to select multiple

--- raumreservierung-frontend/src/views/bookings/BookingsCalendarView.vue
+++ raumreservierung-frontend/src/views/bookings/BookingsCalendarView.vue
@@ -10,6 +10,17 @@
       />
     </template>
     <template #default>
+      <room-select
+        v-model="selectedRooms"
+        :label="
+          t('generics.select', {
+            domain: t('domain.room.header', { count: 2 }),
+          })
+        "
+        class="mb-4"
+        multiple
+        :rules="[rules.required('Bitte wählen Sie mindestens einen Raum')]"
+      />
       <booking-details-summary
         :booking="getBookingData"
         :loading="getBookingLoading"
@@ -20,19 +31,24 @@
 
 <script setup lang="ts">
 import { mdiArrowLeft } from "@mdi/js";
-import { computed } from "vue";
+import { computed, ref } from "vue";
 import { useI18n } from "vue-i18n";
 import { useRoute, useRouter } from "vue-router";
 
 import BookingDetailsSummary from "@/components/booking/BookingDetailsSummary.vue";
 import BaseView from "@/components/common/BaseView.vue";
+import RoomSelect from "@/components/rooms/RoomSelect.vue";
 import { useGetBookingTS } from "@/composables/api/useBookingsApi.ts";
+import { useRules } from "@/composables/useRules.ts";
 
 const { t } = useI18n();
 const route = useRoute();
 const router = useRouter();
+const rules = useRules();
 
 const bookingId = computed(() => (route.params.id as string) || undefined);
+
+const selectedRooms = ref();
 
 const { data: getBookingData, isLoading: getBookingLoading } = useGetBookingTS(
   bookingId.value
