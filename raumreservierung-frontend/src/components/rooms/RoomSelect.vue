8c30be7df1b6a0ee53f1b39ff851404ecd3843e3
langehm <38280183+langehm@users.noreply.github.com>
2026-08-11 15:50:25 +0200

    :sparkles: remodeled roomSelect to emit the whole roomData which are selected

--- raumreservierung-frontend/src/components/rooms/RoomSelect.vue
+++ raumreservierung-frontend/src/components/rooms/RoomSelect.vue
@@ -2,25 +2,40 @@
   <v-select
     v-model="modelValue"
     v-bind="$attrs"
+    :multiple="multiple"
     :prepend-inner-icon="mdiDoor"
     :loading="getRoomsLoading || loading"
-    :items="filteredRooms ?? []"
+    :items="filteredRooms"
     item-value="id"
     item-title="name"
     variant="outlined"
     :disabled="getRoomsLoading || loading"
     hide-details="auto"
-    :multiple="multiple"
+    @update:model-value="emitRoomData"
   />
 </template>
 
 <script setup lang="ts" generic="IsMultiple extends boolean = false">
+import type { RoomListResponseDTO } from "@/api/raumreservierung-backend";
+
 import { mdiDoor } from "@mdi/js";
 import { computed } from "vue";
 
 import { useGetAllRooms } from "@/composables/api/useRoomsApi.ts";
 
+type RoomDataPayload = IsMultiple extends true
+  ? RoomListResponseDTO[]
+  : RoomListResponseDTO | undefined;
+
 const modelValue = defineModel<IsMultiple extends true ? string[] : string>();
+
+const emit = defineEmits<{
+  "update:roomData": [
+    data: IsMultiple extends true
+      ? RoomListResponseDTO[]
+      : RoomListResponseDTO | undefined,
+  ];
+}>();
 
 const {
   loading = false,
@@ -32,16 +47,46 @@ const {
   multiple?: IsMultiple;
 }>();
 
-const filteredRooms = computed(
-  () =>
-    allRooms?.value?.filter(
-      (room) => showInactive || room.isActive || room.id === modelValue.value
-    ) || []
-);
-
 const { data: allRooms, isPending: getRoomsLoading } = useGetAllRooms({
   onlyActive: false,
 });
-</script>
 
-<style scoped></style>
+// Seems unnecessary but is not. multiple is not a simple type anymore, therefore the vue compiler cannot map `multiple` in the template to `multiple="true"` and maps it as a string.
+const isMultipleProp = computed(
+  () => multiple === true || (multiple as unknown) === ""
+);
+
+const filteredRooms = computed(() => {
+  if (!allRooms?.value) {
+    return [];
+  }
+
+  return allRooms.value.filter((room) => {
+    if (showInactive || room.isActive) {
+      return true;
+    }
+
+    if (multiple && Array.isArray(modelValue.value)) {
+      return room.id ? modelValue.value.includes(room.id) : false;
+    }
+
+    return room.id === modelValue.value;
+  });
+});
+
+/**
+ * Emits the roomData of the selected room / rooms
+ */
+const emitRoomData = (value: string | string[] | null | undefined) => {
+  const selectedRoomData =
+    isMultipleProp.value && Array.isArray(value)
+      ? (allRooms.value?.filter((room) => {
+          return room.id && value.includes(room.id);
+        }) ?? [])
+      : allRooms.value?.find((room) => {
+          return room.id === value;
+        });
+
+  emit("update:roomData", selectedRoomData as RoomDataPayload);
+};
+</script>
