52ff01860a2eed86d71333e3dba57038330e7b59
langehm <38280183+langehm@users.noreply.github.com>
2026-08-11 11:06:31 +0200

    :sparkles: added simple component for displaying text next to icon

---
+++ raumreservierung-frontend/src/components/common/IconInformation.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="d-flex align-center">
+    <v-icon
+      :icon="icon"
+      class="mr-2"
+    />
+    <span>{{ text }}</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  icon?: string;
+  text?: string;
+}>();
+</script>
+
+<style scoped></style>
