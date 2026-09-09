0aba2164dc30467cf95d03b2e5ee7247690f72a1
langehm <38280183+langehm@users.noreply.github.com>
2026-09-09 09:46:54 +0200

    added status to bookingMinimal

--- raumreservierung-backend/src/main/java/de/muenchen/raumreservierung/booking/dto/BookingMinimalResponseDTO.java
+++ raumreservierung-backend/src/main/java/de/muenchen/raumreservierung/booking/dto/BookingMinimalResponseDTO.java
@@ -1,12 +1,15 @@
 package de.muenchen.raumreservierung.booking.dto;
 
+import de.muenchen.raumreservierung.booking.BookingStatus;
 import de.muenchen.raumreservierung.person.dto.PersonResponseDto;
 import jakarta.validation.constraints.NotNull;
+
 import java.util.UUID;
 
 public record BookingMinimalResponseDTO(
         @NotNull UUID id,
         @NotNull String title,
+        @NotNull BookingStatus status,
         @NotNull PersonResponseDto bookedBy,
         @NotNull PersonResponseDto bookedFor,
         UUID roomId) {
