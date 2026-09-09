cd1012f40909cf6a33541a3c98731de95b4a1c72
langehm <38280183+langehm@users.noreply.github.com>
2026-09-09 09:42:26 +0200

    partially switched to tanstack

--- raumreservierung-frontend/src/composables/api/useAppointmentApi.ts
+++ raumreservierung-frontend/src/composables/api/useAppointmentApi.ts
@@ -4,10 +4,16 @@ import type {
   PagedModelAppointmentDetailsResponseDTO,
   UpdateAppointmentRequest,
 } from "@/api/raumreservierung-backend";
+import type { MaybeRefOrGetter } from "vue";
+
+import { keepPreviousData, useQuery } from "@tanstack/vue-query";
+import { computed, toValue } from "vue";
 
 import { AppointmentControllerApi } from "@/api/raumreservierung-backend";
 import { useApi } from "@/composables/api/useApi.ts";
 import { ApiFactory } from "@/util/apiFactory.ts";
+
+const APPOINTMENTS_KEY = "appointment";
 
 export const useUpdateAppointment = () => {
   const api = ApiFactory.getInstance(AppointmentControllerApi);
@@ -17,11 +23,37 @@ export const useUpdateAppointment = () => {
   );
 };
 
-export const useGetAppointments = () => {
+export const useGetAppointmentsOld = () => {
   const api = ApiFactory.getInstance(AppointmentControllerApi);
 
   return useApi<
     GetAppointmentsByPageableAndFilterRequest,
     PagedModelAppointmentDetailsResponseDTO
   >((params) => api.getAppointmentsByPageableAndFilter(params));
+};
+
+export const useGetAppointments = (
+  params: MaybeRefOrGetter<
+    GetAppointmentsByPageableAndFilterRequest | undefined
+  >
+) => {
+  const api = ApiFactory.getInstance(AppointmentControllerApi);
+  const paramsRef = computed(() => toValue(params));
+
+  return useQuery({
+    queryKey: [APPOINTMENTS_KEY, paramsRef],
+    queryFn: async () => {
+      const currentParams = paramsRef.value;
+
+      if (!currentParams?.startDate || !currentParams?.endDate) {
+        throw new Error("startDate and endDate are required");
+      }
+
+      return await api.getAppointmentsByPageableAndFilter(currentParams);
+    },
+    enabled: computed(
+      () => !!paramsRef.value?.startDate && !!paramsRef.value?.endDate
+    ),
+    placeholderData: keepPreviousData,
+  });
 };
