350e84e4d1960ea14faccbe6b37a78c1aa8c6a57
langehm <38280183+langehm@users.noreply.github.com>
2026-09-09 09:46:54 +0200

    added status to bookingMinimal

--- raumreservierung-backend/src/main/java/de/muenchen/raumreservierung/appointment/dto/AppointmentFilterDTO.java
+++ raumreservierung-backend/src/main/java/de/muenchen/raumreservierung/appointment/dto/AppointmentFilterDTO.java
@@ -1,15 +1,18 @@
 package de.muenchen.raumreservierung.appointment.dto;
 
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import io.swagger.v3.oas.annotations.Parameter;
 import jakarta.validation.constraints.NotNull;
+
 import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.UUID;
 
-@SuppressFBWarnings(value = { "EI_EXPOSE_REP" }, justification = "DTOs are simple data carriers")
+@SuppressFBWarnings(value = {"EI_EXPOSE_REP"}, justification = "DTOs are simple data carriers")
 public record AppointmentFilterDTO(
         @NotNull OffsetDateTime startDate,
         @NotNull OffsetDateTime endDate,
         UUID bookingId,
+        @Parameter(hidden = true)
         List<UUID> roomIds) {
 }
