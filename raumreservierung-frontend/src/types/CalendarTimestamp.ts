17baf11b91eddeec8a86be3b36be3afaef3feeae
langehm <38280183+langehm@users.noreply.github.com>
2026-09-10 13:22:10 +0200

    fixed type import with new type

---
+++ raumreservierung-frontend/src/types/CalendarTimestamp.ts
@@ -0,0 +1,12 @@
+// Recreated type of vuetify calendar as it is only exported internally and cannot be imported therefore
+export interface CalendarTimestamp {
+  year: number;
+  month: number;
+  day: number;
+  hour: number;
+  minute: number;
+  category?: {
+    categoryName: string;
+    name?: string;
+  };
+}
