78b1840d839a753d0c39c2cae672f16f26a83add
langehm <38280183+langehm@users.noreply.github.com>
2026-09-10 13:44:44 +0200

    disabled buttons for calendarView and improved button structure with new button group

---
+++ raumreservierung-frontend/src/components/common/buttons/rrButtonGroup.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-btn-group
+    :density="mobile ? 'default' : 'comfortable'"
+    :divided="mobile"
+    :direction="mobile ? 'vertical' : 'horizontal'"
+  >
+    <slot />
+  </v-btn-group>
+</template>
+
+<script setup lang="ts">
+import { useDisplay } from "vuetify/framework";
+
+const { mobile } = useDisplay();
+</script>
+
+<style scoped></style>
